90e544420b30ee06798ab40addb014316f095a90
habbbe <habbbe@gmail.com>
2016-04-21 15:12:08 +0200

    Adjusted variables to code style Separated calls for fetching tags and length Changed type of variables from int32_t to int

--- src/decoder/plugins/UsfDecoderPlugin.cxx
+++ src/decoder/plugins/UsfDecoderPlugin.cxx
@@ -36,33 +36,33 @@ static constexpr unsigned USF_CHANNELS = 2;
 static constexpr unsigned USF_BUFFER_SAMPLES = USF_BUFFER_FRAMES*USF_CHANNELS;
 
 static void *
-stdio_fopen( const char * path )
+stdio_fopen(const char *path)
 {
-    return fopen( path, "rb" );
+    return fopen(path, "rb");
 }
 
 static size_t
-stdio_fread( void *p, size_t size, size_t count, void *f )
+stdio_fread(void *p, size_t size, size_t count, void *f)
 {
-    return fread( p, size, count, (FILE*) f );
+    return fread(p, size, count, (FILE *) f);
 }
 
 static int
-stdio_fseek( void * f, int64_t offset, int whence )
+stdio_fseek(void *f, int64_t offset, int whence)
 {
-    return fseek( (FILE*) f, offset, whence );
+    return fseek((FILE *) f, offset, whence);
 }
 
 static int
-stdio_fclose( void * f )
+stdio_fclose(void *f)
 {
-    return fclose( (FILE*) f );
+    return fclose((FILE *) f);
 }
 
 static long
 stdio_ftell( void * f )
 {
-    return ftell( (FILE*) f );
+    return ftell((FILE *) f);
 }
 
 static constexpr psf_file_callbacks stdio_callbacks =
@@ -77,108 +77,122 @@ static constexpr psf_file_callbacks stdio_callbacks =
 
 struct UsfLoaderState
 {
-    bool enableCompare; // The _enablecompare tag is present in the file; passed to usf_set_compare
-    bool enableFIFOFull; // The _enableFIFOfull tag is present in the file; passed to usf_set_fifo_full
-    void * emu; // The emulator state
+    bool enable_compare; // The _enablecompare tag is present in the file; passed to usf_set_compare
+    bool enable_fifo_full; // The _enableFIFOfull tag is present in the file; passed to usf_set_fifo_full
+    void *emu; // The emulator state
 };
 
 static int
-usf_loader(void * context, const uint8_t * exe, size_t exe_size,
-               const uint8_t * reserved, size_t reserved_size)
+usf_loader(void *context, const uint8_t *exe, size_t exe_size,
+               const uint8_t *reserved, size_t reserved_size)
 {
-    struct UsfLoaderState * state = ( struct UsfLoaderState * ) context;
+    struct UsfLoaderState *state = ( struct UsfLoaderState * ) context;
 
-    if ( exe && exe_size > 0 ) return -1;
+    if (exe && exe_size > 0) return -1;
 
-    return usf_upload_section( state->emu, reserved, reserved_size );
+    return usf_upload_section(state->emu, reserved, reserved_size);
 }
 
 static int
-usf_info(void * context, const char * name, const char * value)
+usf_info(void *context, const char *name, const char *value)
 {
-    struct UsfLoaderState * state = ( struct UsfLoaderState * ) context;
+    struct UsfLoaderState *state = (struct UsfLoaderState *) context;
     if (!value) return 0;
 
     if ( strcmp( name, "_enablecompare" ) == 0)
-        state->enableCompare = 1;
+        state->enable_compare = 1;
     else if ( strcmp( name, "_enableFIFOfull" ) == 0)
-        state->enableFIFOFull = 1;
+        state->enable_fifo_full = 1;
 
     return 0;
 }
 
-struct UsfTags {
+struct UsfLengths {
     double length = -1; // Track duration. -1 represents looping infinitely
-    double fade = 0;   // Fade out duration
-    bool setTags = false;  // If set, set the tags with the below variables
-    const TagHandler * tagHandler;
-    void * handler_ctx;
+    double fade = 0;    // Fade out duration
+};
+
+struct UsfTags {
+    UsfLengths &lengths;                  // Song lengths needs to be stored for further calculation
+    const TagHandler &tag_handler; // These two are needed for setting tags when this struct is used
+    void *handler_ctx;                    // as context for tag parsing
+    UsfTags(UsfLengths &lens, const TagHandler &handler) : lengths(lens), tag_handler(handler) {} 
 };
 
 static double
-get_length_from_string(const char * string)
+get_length_from_string(const char *string)
 {
     size_t len = strlen(string);
-    int32_t total = 0; // Total time in milliseconds
-    int32_t finalMult = 1000; // Multiplier for the final unit. If no delimiter is used, use seconds.
-    int32_t localMult = 1; // Multiplier in the currently parsed unit (millisecond, second, minute).
-    int32_t tmp = 0; // 
+    int total = 0;         // Total time in milliseconds
+    int final_mult = 1000; // Multiplier for the final unit. If no delimiter is used, use seconds.
+    int local_mult = 1;    // Multiplier in the currently parsed unit (millisecond, second, minute).
+    int acc = 0;           // The accumulating value of the current unit
     for (int i = len - 1; i >= 0; i--) {
         char c = string[i];
         if (c >= '0' && c <= '9') {
-            tmp += (c - '0') * localMult;
-            localMult *= 10;
+            acc += (c - '0') * local_mult;
+            local_mult *= 10;
         } else {
-            localMult = 1;
+            local_mult = 1;
             int mult = 0;
             if (c == '.') {
                 mult = 1;
             } else if (c == ':') {
                 mult = 1000;
-                finalMult = 60000;
+                final_mult = 60000;
             } else {
                 return -1; // Error in parsing duration. Return -1 (looping).
             }
-            total += tmp * mult;
-            tmp = 0;
+            total += acc * mult;
+            acc = 0;
         }
     }
-    total += finalMult*tmp;
+    total += final_mult*acc;
 
     return total/1000;
 }
 
+static void
+set_length_from_tags(UsfLengths &lengths, const char *name, const char *value) {
+    if (strcmp("length", name) == 0) {
+        lengths.length = get_length_from_string(value);
+    } else if (strcmp("fade", name) == 0) {
+        lengths.fade = get_length_from_string(value);
+    }
+}
+
+static int
+usf_lengths_target(void *context, const char *name, const char *value)
+{
+    struct UsfLengths *lengths = (struct UsfLengths *) context;
+    set_length_from_tags(*lengths, name, value);
+    return 0;
+}
+
 static bool
-usf_set_tag(const char * field, const char * name, const char *value, TagType type, UsfTags * tags)
+usf_set_tag(const char *field, const char *name, const char *value, TagType type, UsfTags *tags)
 {
     if (strcmp(name, field) == 0) {
-        tag_handler_invoke_tag(*tags->tagHandler, tags->handler_ctx, type, value);
+        tag_handler_invoke_tag(tags->tag_handler, tags->handler_ctx, type, value);
         return true;
     }
     return false;
 }
 
 static int
-usf_tags_target(void * context, const char * name, const char * value)
+usf_tags_target(void *context, const char *name, const char *value)
 {
-    struct UsfTags * tags = ( struct UsfTags * ) context;
-    if (!value) return 0;
+    struct UsfTags *tags = (struct UsfTags*) context;
 
-    if (tags->setTags) {
-        usf_set_tag("title", name, value, TAG_TITLE, tags) ||
+    usf_set_tag("title", name, value, TAG_TITLE, tags) ||
             usf_set_tag("artist", name, value, TAG_ARTIST, tags) ||
             usf_set_tag("composer", name, value, TAG_COMPOSER, tags) ||
             usf_set_tag("game", name, value, TAG_ALBUM, tags) ||
             usf_set_tag("year", name, value, TAG_DATE, tags) ||
             usf_set_tag("genre", name, value, TAG_GENRE, tags) ||
             usf_set_tag("track", name, value, TAG_TRACK, tags);
-    }
+    set_length_from_tags(tags->lengths, name, value);
 
-    if (strcmp("length", name) == 0) {
-        tags->length = get_length_from_string(value);
-    } else if (strcmp("fade", name) == 0) {
-        tags->fade = get_length_from_string(value);
-    }
     return 0;
 }
 
@@ -195,16 +209,16 @@ usf_file_decode(Decoder &decoder, Path path_fs)
 {
     /* Load the file */
 
-    UsfTags tags;
+    UsfLengths lengths;
     UsfLoaderState state;
     state.emu = malloc(usf_get_state_size());
     usf_clear(state.emu);
-    AtScopeExit(state) {
+    AtScopeExit(&state) {
         free(state.emu);
     };
 
     const char* path = path_fs.c_str();
-    const int psf_version_tags = psf_load(path, &stdio_callbacks, 0, 0, 0, usf_tags_target, &tags, 0);
+    const int psf_version_tags = psf_load(path, &stdio_callbacks, 0, 0, 0, usf_lengths_target, &lengths, 0);
     const int psf_version_state = psf_load( path, &stdio_callbacks, psf_version_tags, usf_loader, &state, usf_info, &state, 0 );
 
     if ( psf_version_tags != 0x21 || psf_version_state <= 0 ) {
@@ -212,21 +226,21 @@ usf_file_decode(Decoder &decoder, Path path_fs)
         return;
     }
 
-    usf_set_compare( state.emu, state.enableCompare );
-    usf_set_fifo_full( state.emu, state.enableFIFOFull );
+    usf_set_compare(state.emu, state.enable_compare);
+    usf_set_fifo_full(state.emu, state.enable_fifo_full);
 
     int32_t sample_rate;
-    usf_render( state.emu, 0, 0, &sample_rate );
+    usf_render(state.emu, 0, 0, &sample_rate);
 
     /* initialize the MPD decoder */
 
     const AudioFormat audio_format(sample_rate, SampleFormat::S16, USF_CHANNELS);
     assert(audio_format.IsValid());
 
-    double totalLength =  track_lengths(tags.length, tags.fade);
+    double total_length =  track_lengths(lengths.length, lengths.fade);
 
     // Duration
-    decoder_initialized(decoder, audio_format, true, SongTime::FromS(totalLength));
+    decoder_initialized(decoder, audio_format, true, SongTime::FromS(total_length));
 
     /* .. and play */
     DecoderCommand cmd;
@@ -239,27 +253,27 @@ usf_file_decode(Decoder &decoder, Path path_fs)
         }
 
         // Simple fading
-        double fadeTime = tags.fade;
-        double trackLength = tags.length;
-        if (fadeTime > 0 && totalLength >= 0 && decoder.timestamp > trackLength) {
-            const double vol = 1.0 - ((decoder.timestamp - trackLength) / fadeTime);
-            const double normalizedVol = vol < 0 ? 0 : vol;
+        double fade_time = lengths.fade;
+        double track_length = lengths.length;
+        if (fade_time > 0 && total_length >= 0 && decoder.timestamp > track_length) {
+            const double vol = 1.0 - ((decoder.timestamp - track_length) / fade_time);
+            const double normalized_vol = vol < 0 ? 0 : vol;
             for (unsigned int i = 0; i < USF_BUFFER_SAMPLES; i++) {
-                buf[i] *= normalizedVol;
+                buf[i] *= normalized_vol;
             }
         }
 
         cmd = decoder_data(decoder, nullptr, buf, sizeof(buf), 0);
 
         // Stop song manually
-        if (totalLength >= 0 && decoder.timestamp > totalLength+2)
+        if (total_length >= 0 && decoder.timestamp > total_length+2)
             break;
 
         if (cmd == DecoderCommand::SEEK) {
             // If user seeks during the fade period. Disable fading and play forever.
             // Hacky way to give user posibility to enable looping on the fly
-            if (decoder.timestamp > trackLength) {
-                totalLength = -1;
+            if (decoder.timestamp > track_length) {
+                total_length = -1;
             }
             // Seek manually by restarting emulator and discarding samples.
             const double target_time = decoder_seek_time(decoder).ToDoubleS();
@@ -281,9 +295,8 @@ static bool
 usf_scan_file(Path path_fs, const struct TagHandler &handler, void *handler_ctx) 
 {
     const char* path = path_fs.c_str();
-    UsfTags tags;
-    tags.setTags = true;
-    tags.tagHandler = &handler;
+    UsfLengths lengths;
+    UsfTags tags(lengths, handler);
     tags.handler_ctx = handler_ctx;
     const int psf_version = psf_load(path, &stdio_callbacks, 0, 0, 0, usf_tags_target, &tags, 0);
     if (psf_version < 0) {
@@ -291,9 +304,10 @@ usf_scan_file(Path path_fs, const struct TagHandler &handler, void *handler_ctx)
     }
 
     // Duration
-    double totalLength = track_lengths(tags.length, tags.fade);
+    double total_length = track_lengths(lengths.length, lengths.fade);
+    printf("Length: %f\n", total_length);
 
-    tag_handler_invoke_duration(handler, handler_ctx, SongTime::FromS(totalLength));
+    tag_handler_invoke_duration(handler, handler_ctx, SongTime::FromS(total_length));
     return true;
 }
 
